3ac610279193d7d0c0d2ff48d08554a26eb1378b
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-18 09:03:19 -0300

    feat: tracking component

---
+++ src/presentation/components/tracking/index.tsx
@@ -0,0 +1,14 @@
+import style from './index.module.css';
+
+const Tracking: React.FC = () => {
+    return (
+        <div className={style.containerTracking}>
+            <div className={style.boxTracking}>
+
+                Hello tracking
+            </div>
+        </div>
+    )
+}
+
+export default Tracking;
