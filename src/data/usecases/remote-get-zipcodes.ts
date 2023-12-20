155652d5f1d80278b2427ac3e13377187e618aff
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-20 11:24:34 -0300

    feat: class remote get zipcodes

---
+++ src/data/usecases/remote-get-zipcodes.ts
@@ -0,0 +1,24 @@
+import { GetZipcodesError } from "@/domain/error/get-zipcodes-error";
+import { Zipcodes } from "@/domain/models/get-zipcodes";
+import { GetZipcodes } from "@/domain/usecases/get-zipcodes";
+import { HttpClient, HttpStatusCode } from "../protocols/http";
+
+export class RemoteGetZipcodes implements GetZipcodes {
+    constructor(
+        private httpClient: HttpClient
+    ) { };
+
+    async getZipcode(): Promise<Zipcodes> {
+        const response = await this.httpClient.request({
+            method: 'get',
+            url: `${import.meta.env.VITE_URL_API_ZIPCODE}/zipcode`,
+        });
+
+        if (!response ||
+            response.statusCode !== HttpStatusCode.Ok && response.statusCode !== HttpStatusCode.NOCONTENT
+        )
+            throw new GetZipcodesError(response?.body);
+
+        return response.body;
+    }
+}
