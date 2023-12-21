298dfc92da43d7f244b9c7a55ee77a1efaebbc85
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-21 07:50:26 -0300

    fix: alter import factory

--- src/main/routes/route.tsx
+++ src/main/routes/route.tsx
@@ -1,7 +1,7 @@
 import { EnumRoutes } from '@/domain/enums';
 import MakeCreateAccount from '@/main/factories/pages/create-account-factory';
+import MakeHome from '@/main/factories/pages/home-factory';
 import MakeLogin from '@/main/factories/pages/login-factory';
-import MakeHome from '@/presentation/pages/home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PrivateRoute from '../proxies/private-route';
 
