8938b4885742b8e66c404448cf4a40020daa3939
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-12-21 07:29:59 -0300

    feat: import factory to component home

--- src/main/factories/pages/home-factory.tsx
+++ src/main/factories/pages/home-factory.tsx
@@ -1,9 +1,10 @@
 import Home from "@/presentation/pages/home";
 import { memo } from "react";
+import { makeRemoteGetZipcodes } from "../usecases/remote-get-zipcodes";
 
 const MakeHome: React.FC = () => {
     return (
-        <Home />
+        <Home getZipcodes={makeRemoteGetZipcodes()} />
     )
 }
 
