ee92c0fac2de1e7aea36096fba94ef73dfcd78db
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-01-08 18:35:02 -0300

    feat: remote add account class

--- tests/domain/mocks/mock-account.ts
+++ tests/domain/mocks/mock-account.ts
@@ -1,7 +1,15 @@
 
-import * as faker from 'faker'
-import { ResponseLoginAccount } from '../usecases'
+import * as faker from 'faker';
+import { RequestAddAccount, ResponseLoginAccount } from '../usecases';
 
 export const mockAccountModel = (): ResponseLoginAccount => ({
     token: faker.datatype.uuid(),
-})
+});
+
+export const mockRequestAddAccount = (): RequestAddAccount => {
+    return {
+        email: faker.internet.email(),
+        password: faker.random.words(),
+        username: faker.name.firstName()
+    };
+};
