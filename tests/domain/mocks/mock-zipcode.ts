4b0eacee5c6156420d594ed2c252f74c87fa46d2
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-01-09 18:40:40 -0300

    test: create a mockResponseGetZipcodes

--- tests/domain/mocks/mock-zipcode.ts
+++ tests/domain/mocks/mock-zipcode.ts
@@ -1,9 +1,30 @@
 import faker from "faker";
 import { CreateZipcodeModel } from "../models/create-zipcodes";
+import { Zipcodes } from "../models/get-zipcodes";
 
 export const mockRequestCreateZipcode = (): CreateZipcodeModel => {
     return {
         name: faker.name.firstName(),
-        zipcode: faker.address.zipCode()
+        zipcode: faker.datatype.uuid()
+    }
+};
+
+export const mockResponseGetZipcodes = (): Zipcodes => {
+    return {
+        zipcodes: [
+            {
+                code: faker.datatype.uuid(),
+                name: faker.random.words(),
+                routes: [
+                    {
+                        date: faker.date.recent().toString(),
+                        description: faker.datatype.string(),
+                        end: faker.address.cityName(),
+                        start: faker.address.cityName()
+                    }
+                ],
+                status: faker.random.words()
+            }
+        ]
     }
 };
