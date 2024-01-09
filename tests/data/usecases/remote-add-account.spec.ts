a0bef806d6cded987a0dd9bb027b99faab3d24c1
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-01-09 18:19:43 -0300

    test: Should throw error if statusCode different 201 in RemoteAddAccount

--- tests/data/usecases/remote-add-account.spec.ts
+++ tests/data/usecases/remote-add-account.spec.ts
@@ -1,5 +1,6 @@
 import { HttpStatusCode } from "@/data/protocols/http";
 import { RemoteAddAccount } from "@/data/usecases/remote-add-account";
+import { CreateAccountError } from "@/domain/error/create-account-error";
 import { describe, expect, it } from "vitest";
 import { mockRequestAddAccount } from "../../domain/mocks/mock-account";
 import { HttpClientSpy } from "../mocks";
@@ -19,7 +20,7 @@ const makeSut = (): Props => {
 };
 
 describe('RemoteAddAccount', () => {
-    it('Should correct values', async () => {
+    it('Should correct values', () => {
         const { sut, httpClientSpy } = makeSut();
 
         httpClientSpy.response.statusCode = HttpStatusCode.Created;
@@ -31,5 +32,15 @@ describe('RemoteAddAccount', () => {
         expect(httpClientSpy.body).toEqual(request);
         expect(httpClientSpy.method).toBe('post');
         expect(httpClientSpy.url).toBe(`${import.meta.env.VITE_URL_API_AUTHENTICATION}/create_account`);
-    })
+    });
+
+    it('Should throw error if statusCode different 201', () => {
+        const { sut, httpClientSpy } = makeSut();
+
+        httpClientSpy.response.statusCode = HttpStatusCode.BadRequest;
+
+        const request = mockRequestAddAccount();
+
+        expect(sut.add(request)).rejects.toThrow(CreateAccountError);
+    });
 })
