24f94f62eb0a926360f129967f53ca3e004112ca
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2024-01-09 18:49:38 -0300

    test: Should correct values in RemoteLoginAccount

---
+++ tests/data/usecases/remote-login-account.spec.ts
@@ -0,0 +1,40 @@
+import { HttpStatusCode } from "@/data/protocols/http";
+import { RemoteLoginAccount } from "@/data/usecases/remote-login-account";
+import { describe, expect, it } from "vitest";
+import { mockRequestLoginAccount, mockResponseLoginAccount } from "../../domain/mocks/mock-account";
+import { HttpClientSpy } from "../mocks";
+
+type Props = {
+    sut: RemoteLoginAccount;
+    httpClientSpy: HttpClientSpy;
+};
+
+const makeSut = (): Props => {
+    const httpClientSpy = new HttpClientSpy();
+    const sut = new RemoteLoginAccount(httpClientSpy);
+
+    return {
+        sut,
+        httpClientSpy
+    }
+};
+
+describe('RemoteLoginAccount', () => {
+    it('Should correct values', async () => {
+        const { sut, httpClientSpy } = makeSut();
+
+        const responseMock = mockResponseLoginAccount();
+
+        httpClientSpy.response.statusCode = HttpStatusCode.Created;
+        httpClientSpy.response.body = responseMock;
+
+        const request = mockRequestLoginAccount();
+
+        const response = await sut.auth(request);
+
+        expect(httpClientSpy.body).toEqual(request);
+        expect(httpClientSpy.method).toBe('post');
+        expect(httpClientSpy.url).toBe(`${import.meta.env.VITE_URL_API_AUTHENTICATION}/login_account`);
+        expect(response).toEqual(responseMock);
+    });
+});
